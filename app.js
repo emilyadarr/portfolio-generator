4a0e97b792c1b823639d73065527958c292ecf78
Emily <emilyadarr@gmail.com>
2022-01-25 09:10:33 -0500

    add inquirer

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-// console.log(profileDataArgs);
-// const printProfileData = profileDataArr => {
-//   // This ...
-//   for (let i = 0; i < profileDataArr.length; i++) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this ...
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem)
-//   });
-// };
-
-// printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
